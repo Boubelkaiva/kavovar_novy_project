91604c574348bf742d5f0edec0473454f5551003
Boubelkaiva <ivana.havlickova@centrum.cz>
2023-12-29 14:28:06 +0100

    Otočení logiky podmínek
    prohození logiky podmínky, nejdříve if - pokud je true, tak uvař, else - vypiš hlášku

--- data.js
+++ data.js
@@ -1,11 +1,3 @@
-// export default let kava = [
-//   { druh: "Černá káva", cena: 25, id: "volba_cerna" },
-//   { druh: "Espresso", cena: 35, id: "volba_espresso" },
-//   { druh: "Cappuccino", cena: 15, id: "volba_cappuccino" },
-//   { druh: "Caffe latte", cena: 30, id: "volba_caffe_latte" },
-//   { druh: "Latte macchiato", cena: 40, id: "volba_latte_macchiato" },
-//   { druh: "Ristretto", cena: 45, id: "volba_ristretto" }
-// ];
 const kava = [
   { druh: "Černá káva", cena: 25, id: "volba_cerna" },
   { druh: "Espresso", cena: 35, id: "volba_espresso" },
